c44fbf68c859de7b31e47c38885374bb44024d4f
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-12 23:54:41 -0300

    Crear Datos A Través de HTTP POST

--- backend/src/main/java/com/ventas/controller/ClienteController.java
+++ backend/src/main/java/com/ventas/controller/ClienteController.java
@@ -3,9 +3,12 @@ package com.ventas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +31,10 @@ public class ClienteController {
 	public ResponseEntity<Cliente> findbyId(@PathVariable("id") Integer idCliente) {
 		return clienteService.findById(idCliente).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
 
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cliente> create(@RequestBody Cliente cliente) {
+		return new ResponseEntity<>(clienteService.create(cliente), HttpStatus.CREATED);
 	}
 }
