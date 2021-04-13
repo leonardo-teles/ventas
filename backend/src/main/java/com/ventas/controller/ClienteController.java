7b91752e7588f63c8421bb158ff097c596b89724
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-13 00:07:53 -0300

    Modificar Datos A Través de HTTP PUT

--- backend/src/main/java/com/ventas/controller/ClienteController.java
+++ backend/src/main/java/com/ventas/controller/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,5 +37,12 @@ public class ClienteController {
 	@PostMapping
 	public ResponseEntity<Cliente> create(@RequestBody Cliente cliente) {
 		return new ResponseEntity<>(clienteService.create(cliente), HttpStatus.CREATED);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Cliente> update(@RequestBody Cliente cliente) {
+		return clienteService.findById(cliente.getIdCliente())
+				.map(c -> ResponseEntity.ok(clienteService.update(cliente)))
+				.orElseGet(() -> ResponseEntity.notFound().build());
 	}
 }
