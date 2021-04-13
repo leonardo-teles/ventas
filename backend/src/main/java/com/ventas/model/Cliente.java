e101b4e999d9e11ed983acfc8dba295cfef69497
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-12 22:53:34 -0300

    Mapear el Objeto Cliente

---
+++ backend/src/main/java/com/ventas/model/Cliente.java
@@ -0,0 +1,80 @@
+package com.ventas.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idCliente;
+	
+	@Column(name = "nombre", nullable = false, length = 70)
+	private String nombre;
+	
+	@Column(name = "apellido", nullable = false, length = 150)
+	private String apellido;
+	
+	@Column(name = "direccion", nullable = true, length = 150)
+	private String direccion;
+	
+	@Column(name = "telefono", nullable = true, length = 10)
+	private String telefono;
+	
+	@Column(nullable = false, length = 150)
+	private String email;
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
