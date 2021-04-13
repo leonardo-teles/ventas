bbf7990fbd656cedc8310e955ba0a00af930393e
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-12 23:45:32 -0300

    Exponer un Recurso Particular A Través de HTTP GET

--- backend/src/main/java/com/ventas/controller/ClienteController.java
+++ backend/src/main/java/com/ventas/controller/ClienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class ClienteController {
 	@GetMapping
 	public ResponseEntity<List<Cliente>> findAll() {
 		return ResponseEntity.ok(clienteService.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> findbyId(@PathVariable("id") Integer idCliente) {
+		return clienteService.findById(idCliente).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
 	}
 }
