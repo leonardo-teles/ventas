2cdcfd5330ee53189c35f7415bb807b99a862e9f
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-13 21:30:39 -0300

    Realizar las Validaciones para los Datos de Entrada y Retornar Código de Estado Correcto

--- backend/src/main/java/com/ventas/controller/ClienteController.java
+++ backend/src/main/java/com/ventas/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.ventas.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -36,12 +38,12 @@ public class ClienteController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Cliente> create(@RequestBody Cliente cliente) {
+	public ResponseEntity<Cliente> create(@Valid @RequestBody Cliente cliente) {
 		return new ResponseEntity<>(clienteService.create(cliente), HttpStatus.CREATED);
 	}
 	
 	@PutMapping
-	public ResponseEntity<Cliente> update(@RequestBody Cliente cliente) {
+	public ResponseEntity<Cliente> update(@Valid @RequestBody Cliente cliente) {
 		return clienteService.findById(cliente.getIdCliente())
 				.map(c -> ResponseEntity.ok(clienteService.update(cliente)))
 				.orElseGet(() -> ResponseEntity.notFound().build());
--- backend/src/main/java/com/ventas/model/Cliente.java
+++ backend/src/main/java/com/ventas/model/Cliente.java
@@ -6,6 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "cliente")
@@ -15,18 +19,30 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idCliente;
 	
+	@NotNull
+	@NotBlank(message = "El nombre es obligatorio")
+	@Size(min = 3, max = 70, message = "El nombre debe ser mayor a 3 caracteres")	
 	@Column(name = "nombre", nullable = false, length = 70)
 	private String nombre;
-	
+
+	@NotNull
+	@NotBlank(message = "El apellido es obligatorio")
+	@Size(min = 3, max = 150, message = "El apellido debe ser mayor a 3 caracteres")	
 	@Column(name = "apellido", nullable = false, length = 150)
 	private String apellido;
 	
+	@Size(max = 150, message = "La dirección no debe superar los 150 caracteres")
 	@Column(name = "direccion", nullable = true, length = 150)
 	private String direccion;
 	
+	@Size(min = 8, message = "La teléfono debe tener al menos 8 caracteres")
 	@Column(name = "telefono", nullable = true, length = 10)
 	private String telefono;
-	
+
+	@NotNull
+	@NotBlank(message = "El e-mail es obligatorio")
+	@Size(min = 10, message = "El e-mail debe ser al menos de 10 caracteres")
+	@Email(message = "El e-mail enviado no es un formato válido")
 	@Column(nullable = false, length = 150)
 	private String email;
 
