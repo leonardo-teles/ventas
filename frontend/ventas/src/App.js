3afe8b63edff46ef03acca63bce1662443cccec4
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-14 23:54:57 -0300

    React-Router-Dom

--- frontend/ventas/src/App.js
+++ frontend/ventas/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/js/all';
 
-import Layout from './components/Layout';
+import Home from './pages/Home';
+import Clientes from './pages/Clientes';
 
 function App() {
   return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/> 
+          <Route exact path="/clientes" component={Clientes}/> 
+        </Switch>
+      </Router>
+    
 
-    <div>
-       <Layout />
-    </div>
-
+    
+   
   );
 }
 
