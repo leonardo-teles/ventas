5037a9c3d3ae84f51682c69c25ddb797c84e5aac
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-15 18:51:13 -0300

    Renderizar Valores del State

--- frontend/ventas/src/components/clientes/TableCliente.js
+++ frontend/ventas/src/components/clientes/TableCliente.js
@@ -3,7 +3,30 @@ import React, { useState } from 'react';
 const TableCliente = () => {
 
     const[clientesList, setClientesList] = useState([
-
+        {
+            "idCliente" : 1,
+            "nombre" : "Leonardo",
+            "apellido" : "Teles de Almeida",
+            "direccion" : "Estrada da Cachamorra",
+            "telefono" : "979615072",
+            "email" : "leonardo.teles.almeida@gmail.com"
+        },
+        {
+            "idCliente" : 2,
+            "nombre" : "Manuela",
+            "apellido" : "Souza de Almeida",
+            "direccion" : "Estrada da Cachamorra",
+            "telefono" : "991054374",
+            "email" : "manuela.souza.almeida@gmail.com"
+        },
+        {
+            "idCliente" : 3,
+            "nombre" : "Juliana",
+            "apellido" : "Ferreira de Almeida",
+            "direccion" : "Rua Murilo Alvarenga",
+            "telefono" : "12345678",
+            "email" : "juliana.ferreira.almeida@gmail.com"
+        }
     ]);
 
     return ( 
@@ -20,25 +43,29 @@ const TableCliente = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            <button className="button is-small is-info mr-1" title="Modificar">
-                                <span className="icon is-small">
-                                    <i className="fas fa-edit"></i>
-                                </span>
-                            </button>
-                            <button className="button is-small is-danger" title="Eliminar">
-                                <span className="icon is-small">
-                                    <i className="fas fa-trash-alt"></i>
-                                </span>
-                            </button>
-                        </td>
-                        <td>Leonardo</td>
-                        <td>Teles de Almeida</td>
-                        <td>Estrada da Cachamorra</td>
-                        <td>979615072</td>
-                        <td>leonardo.teles.almeida@gmail.com</td>
-                    </tr>
+                    {
+                        clientesList.map(cliente => (
+                            <tr key={cliente.idCliente}>
+                                <td>
+                                    <button className="button is-small is-info mr-1" title="Modificar">
+                                        <span className="icon is-small">
+                                            <i className="fas fa-edit"></i>
+                                        </span>
+                                    </button>
+                                    <button className="button is-small is-danger" title="Eliminar">
+                                        <span className="icon is-small">
+                                            <i className="fas fa-trash-alt"></i>
+                                        </span>
+                                    </button>
+                                </td>
+                                <td>{cliente.nombre}</td>
+                                <td>{cliente.apellido}</td>
+                                <td>{cliente.direccion}</td>
+                                <td>{cliente.telefono}</td>
+                                <td>{cliente.email}</td>
+                            </tr>
+                        ))
+                    }
                 </tbody>
             </table>
         </div>
