002a221a11834f1a1a3a6f40007963f93e0a9762
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-14 22:57:13 -0300

    Componente Header

--- frontend/ventas/src/App.js
+++ frontend/ventas/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import 'bulma/css/bulma.css';
+
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-        <h1>App Ventas</h1>
-    </div>
+    <Header />
   );
 }
 
---
+++ frontend/ventas/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <div className="navbar is-primary">
+            <div className="navbar-brand">
+                <a href="/" className="navbar-item">
+                    <img src="" alt="logo"/>
+                </a>
+            </div>
+        </div>
+     );
+}
+ 
+export default Header;
