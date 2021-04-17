399e770ea2268b0794e7c6f5673d5b7c9afb3213
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-04-17 18:45:42 -0300

    Context useReducer - Crear Función Obtener Clientes

--- frontend/ventas/src/context/clienteContext.js
+++ frontend/ventas/src/context/clienteContext.js
@@ -1,4 +1,6 @@
 import React, { createContext, useReducer } from 'react';
+
+import { OBTENER_CLIENTES } from '../const/actionTypes';
 import ClienteReducer from '../reducer/clienteReducer';
 
 export const ClienteContext = createContext();
@@ -10,6 +12,41 @@ export const ClienteContextProvider = props => {
     }
 
     const [state, dispatch] = useReducer(ClienteReducer, initialState);
+
+    const obtenerClientes = () => {
+
+        const clientes = [
+            {
+                "idCliente" : 1,
+                "nombre" : "Leonardo",
+                "apellido" : "Teles de Almeida",
+                "direccion" : "Estrada da Cachamorra",
+                "telefono" : "979615072",
+                "email" : "leonardo.teles.almeida@gmail.com"
+            },
+            {
+                "idCliente" : 2,
+                "nombre" : "Manuela",
+                "apellido" : "Souza de Almeida",
+                "direccion" : "Estrada da Cachamorra",
+                "telefono" : "991054374",
+                "email" : "manuela.souza.almeida@gmail.com"
+            },
+            {
+                "idCliente" : 3,
+                "nombre" : "Juliana",
+                "apellido" : "Ferreira de Almeida",
+                "direccion" : "Rua Murilo Alvarenga",
+                "telefono" : "12345678",
+                "email" : "juliana.ferreira.almeida@gmail.com"
+            }
+        ];
+
+        dispatch({
+            type: OBTENER_CLIENTES,
+            payload: clientes
+        })
+    }
 
     return(
         <ClienteContext.Provider value={{clientesList: state.clientesList}}>
--- frontend/ventas/src/reducer/clienteReducer.js
+++ frontend/ventas/src/reducer/clienteReducer.js
@@ -1,6 +1,13 @@
+import { OBTENER_CLIENTES } from "../const/actionTypes";
+
 export default (state, action) => {
 
     switch (action.type) {
+        case OBTENER_CLIENTES:
+            return {
+                ...state, clientesList: action.payload
+            };
+
         default:
             return state;
     }
